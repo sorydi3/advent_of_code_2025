37a60a597667b3a9f01b4e163a196ddd76a89172
diallo <ibrahimasory22@hotmail.com>
2025-12-04 23:52:02 +0100

    Add utils library with file reading and writing functions

---
+++ utils/src/lib.rs
@@ -0,0 +1,17 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+pub fn read_input(input_file_name: &str) -> std::io::Lines<BufReader<File>> {
+    let file = File::open(input_file_name).unwrap();
+    let reader = BufReader::new(file);
+    reader.lines()
+}   
+
+pub fn write_input(input_file_name: &str) -> std::io::BufWriter<File> {
+    use std::io::BufWriter;
+    let file = File::create(input_file_name).unwrap();
+    let writer = BufWriter::new(file);
+    writer
+}
+
