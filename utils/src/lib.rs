0f4ee4c11a5a9ccc8b5d7dade3c9569c80b70b5c
diallo <ibrahimasory22@hotmail.com>
2025-12-05 23:31:50 +0100

    Fix formatting in lib.rs by removing unnecessary whitespace

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -6,7 +6,7 @@ pub fn read_input(input_file_name: &str) -> std::io::Lines<BufReader<File>> {
     let file = File::open(input_file_name).unwrap();
     let reader = BufReader::new(file);
     reader.lines()
-}   
+}
 
 pub fn write_input(input_file_name: &str) -> std::io::BufWriter<File> {
     use std::io::BufWriter;
@@ -14,4 +14,3 @@ pub fn write_input(input_file_name: &str) -> std::io::BufWriter<File> {
     let writer = BufWriter::new(file);
     writer
 }
-
