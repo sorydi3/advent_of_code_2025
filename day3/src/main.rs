6ee7b46764d8da6e41970f43e984e802b4e96662
diallo <ibrahimasory22@hotmail.com>
2025-12-05 23:33:13 +0100

    Refactor main function to calculate and display joltage from input data

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,10 +1,33 @@
-
-
 use utils::read_input;
 
 fn main() {
-    let banks = read_input("./day3/input.txt"); 
-    println!("Hello, world! {:?}",banks);
+    let banks = read_input("./day3/input.txt");
+    let joltage = banks
+        .map(|bank| {
+            let bateries = bank.unwrap().chars().collect::<Vec<_>>();
+            get_max(&bateries[..])
+        })
+        .sum::<usize>();
+
+    println!("JOLTAGE: {:?}", joltage);
 }
 
-
+fn get_max<'a>(bateries: &'a [char]) -> usize {
+    (0..bateries.len())
+        .map(|i| {
+            let current = bateries[i];
+            bateries
+                .iter()
+                .skip(i+1)
+                .map(move |c| {
+                    [current.to_string(), (*c).to_string()]
+                        .concat()
+                        .parse::<usize>()
+                        .unwrap()
+                })
+                .max()
+                .unwrap_or_default()
+        })
+        .max()
+        .unwrap()
+}
