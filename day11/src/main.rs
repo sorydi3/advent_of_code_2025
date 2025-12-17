88479bf241ad87870fd53b0e70cac6865b6b61aa
diallo <ibrahimasory22@hotmail.com>
2025-12-17 23:05:26 +0100

    Refactor Canva struct: Update node storage to HashMap and improve pathfinding logic

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,4 +1,7 @@
-use std::{path, usize};
+use std::{
+    collections::{BinaryHeap, HashMap, VecDeque},
+    path, usize,
+};
 
 use itertools::Itertools;
 use ui::{App, CustomState, central_panel, eframe};
@@ -7,7 +10,7 @@ use utils::read_input;
 
 type Node = (String, Vec<String>);
 
-type Nodes = Vec<Node>;
+type Nodes = HashMap<String, Node>;
 
 #[derive(Clone, Debug)]
 struct Canva {
@@ -16,14 +19,16 @@ struct Canva {
 
 impl Default for Canva {
     fn default() -> Self {
-        Self { nodes: vec![] }
+        Self {
+            nodes: HashMap::new(),
+        }
     }
 }
 
 impl CustomState for Canva {
     fn new() -> Box<Self> {
-        let nodes = read_input("./day11/input.txt");
-        let vec_nodes = nodes
+        let nodes = read_input("./day11/input1.txt");
+        let mut vec_nodes = nodes
             .map(|line| {
                 line.map(|line| {
                     let node = line
@@ -36,7 +41,7 @@ impl CustomState for Canva {
                                 .filter(|edge| !edge.eq(""))
                                 .collect::<Vec<_>>();
 
-                            (node_name, outputs)
+                            (node_name.clone(), (node_name, outputs))
                         })
                         .expect("MMM");
 
@@ -44,8 +49,8 @@ impl CustomState for Canva {
                 })
                 .expect("MMMM")
             })
-            .collect::<Vec<_>>();
-
+            .collect::<HashMap<_, _>>();
+        vec_nodes.insert("out".into(), ("out".into(), vec![]));
         let res = Self {
             nodes: vec_nodes,
             ..Default::default()
@@ -56,15 +61,51 @@ impl CustomState for Canva {
 
 impl Canva {
     fn get_paths(&self) {
-        let mut visited: Vec<String> = vec![];
-        let init_node = self.nodes.get(1).unwrap();
-        let mut paths: Vec<Vec<String>> = vec![];
-        visited.push(init_node.0.clone());
-        self.i_get_path(init_node, &mut visited, &mut paths);
+        /*
+           let mut visited: Vec<String> = vec![];
+           let init_node = self.nodes.get(1).unwrap();
+           let mut paths: Vec<Vec<String>> = vec![];
+           visited.push(init_node.0.clone());
+           //self.i_get_path(init_node, &mut visited, &mut paths);
 
-        println!("NUM_PATHS: {}", paths.iter().count());
+           println!("NUM_PATHS: {}", paths.iter().count());
 
-        println!("PATHS: {:?}",paths);
+           println!("PATHS: {:?}",paths);
+        */
+    }
+
+    fn get_node(&self, node_name: &String) -> Option<&(String, Vec<String>)> {
+        self.nodes.get(node_name)
+    }
+
+    fn get_path_v2(&mut self) {
+        let init_node = self.nodes.get("you").unwrap();
+        let mut satck: VecDeque<&(String, Vec<String>)> = VecDeque::from([init_node]);
+        let mut visited: Vec<&String> = Vec::new();
+
+        while let Some((candidate_name, candidate_neight)) = satck.pop_back() {
+            println!("NAME: {:?} candidates: {:?} ",candidate_name,candidate_neight);
+            if !visited.contains(&candidate_name) {
+                visited.push(candidate_name);
+                for neig in candidate_neight.iter() {
+                    if neig.eq("out") {
+                        self.get_node(neig).map(|node| {
+                            visited.push(neig);
+                            println!("PATH FOUND");
+                            println!("{:?}", visited)
+                        });
+                    }else {
+                        if !visited.contains(&neig) {
+                            let node = self.get_node(neig);
+                            node.map(|node| {
+                                satck.push_back(node);
+                            });
+                        }
+                    }
+
+                }
+            }
+        }
     }
 
     fn get_pos_candidate(&self, candidate_name: &str) -> Option<usize> {
@@ -79,7 +120,7 @@ impl Canva {
         visited: &mut std::vec::Vec<String>,
         paths: &mut Vec<std::vec::Vec<String>>,
     ) {
-
+        /*
         println!("doing... {:?}",candidates);
         for candidate in candidates.iter() {
             if candidate.eq("out") {
@@ -97,6 +138,7 @@ impl Canva {
                 }
             }
         }
+         */
     }
 }
 
@@ -109,7 +151,7 @@ impl App for Canva {
 
 fn main() {
     let mut canva = Canva::new();
-    canva.get_paths();
+    canva.get_path_v2();
 
     // println!("MIN: {:?}", res);
 }
